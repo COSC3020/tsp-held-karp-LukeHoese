6be1e3c615f42a0be1d3377da02db0cc516969c9
Luke Hoese <lukehoese@gmail.com>
2024-12-08 11:23:13 -0700

    okthisshouldwork

--- code.js
+++ code.js
@@ -1,7 +1,16 @@
 // helper function that takes a list of cities, a start city, our distance matrix, and a set used for memoization, to recursively find the shortest path
 function hk(cities, start, distance_matrix, memoization) {
+    // number of cities in current call
+    let numberOfCities = cities.length;
+
+    // generate a unique memoization key to track if path has already been checked, and return value already calculated if so. chatGPT assisted line to make a unique key by converting arrat to string by joining all elements with a comma and appending our start
+    let memoizationKey = cities.sort().join(",") + "-" + start;
+    if (memoization[memoizationKey] != undefined) {
+        return memoization[memoizationKey];
+    }
+
     // if we only have 2 cities in current call of cities, determine which city is the start and which is the end, and then return the value stored in the distance matrix of the two
-    if (cities.length == 2) {
+    if (numberOfCities == 2) {
         let end;
         if (cities[0] == start) {
             end = cities[1];
@@ -13,27 +22,31 @@ function hk(cities, start, distance_matrix, memoization) {
         return distance_matrix[start][end];
     }
 
-    // create a unique key for our current call of cities and start. got the idea for generating a unique key for each subproblem using this line from chatGPT
-    let memoizationKey = cities.join(",") + "-" + start;
-    // if we've already checked this subproblem, ie there already exists a value stored at this key, return that value
-    if (memoization[memoizationKey] != undefined) {
-        return memoization[memoizationKey];
-    }
-
     // initialize our shortest path to infinity
     let shortestPath = Infinity;
 
     // loop through all cities in our list excluding our start city, as detailed in psuedocode
-    for (let i = 0; i < cities.length; i++) {
-        // set our current city to be city at index i
+    for (let i = 0; i < numberOfCities; i++) {
         let currentCity = cities[i];
         // exclude start city
         if (currentCity == start) {
-            // use fancy new keyword I just learned that skips individual iterations of a loop
             continue;
         }
-        // initialize a new set of cities to be used in recursive call that excludes our current city using a higher order filter function (learned in functional)
-        let unmarkedCities = cities.filter(city => city != currentCity);
+        
+        // initialize array of unmarked cities
+        let unmarkedCities = [];
+
+        // original version of loop below used filter function to create arrays without our current start city to be used in call, but was incorrectly generating. likely user error but nevertheless code below worked much better (aided by DJReflexive's implementation)
+
+        // for each new start position (iteration of outer loop) loop over all other cities
+        for (let j = 0; j < numberOfCities; j++) {
+            // exclude our current start node
+            if (cities[j] == start) {
+                continue;
+            }
+            // add all other cities to unmarked
+            unmarkedCities.push(cities[j]);
+        }
 
         // calculate path distance by recursive call with our new smaller list of cities, and our current city set as the start. add whatever is returned by this call with the distance from our current start to our current city to find path distance
         let pathDistance = hk(unmarkedCities, currentCity, distance_matrix, memoization) + distance_matrix[start][currentCity];
@@ -52,13 +65,46 @@ function hk(cities, start, distance_matrix, memoization) {
 }
 
 function tsp_hk(distance_matrix) {
-    // initialize a list of cities using the keys along the length of the distance matrix
-    let cities = [...Array(distance_matrix.length).keys()];
-    // initialize start to city 0
-    let start = 0;
+    // number of cities
+    let numberOfCities = distance_matrix.length;
+    // base cases, if no cities path is 0, if only 1 city path is 0
+    if (numberOfCities == 0 || numberOfCities == 1) {
+        return 0;
+    }
+    
     // initialize our memoization object as an empty set
     let memoization = {};
+    // initialize variable to track shortest path
+    let shortestPath = Infinity;
+    
+    // replaced original cities array generation with iterative approach that calls helper function for every start position in the original list instead of just once (aided by DJReflexive's implementation)
 
-    // call our helper function with our initial variables, return its result
-    return hk(cities, start, distance_matrix, memoization);
+    // outer loop over all cities to make an array with each unique city as the start
+    for (let start = 0; start < numberOfCities; start++) {
+        let cities = [];
+
+        // inner loop to add all cities to our array
+        for (let city = 0; city < numberOfCities; city++) {
+            cities.push(city);
+        }   
+
+        // call helper function with our start set to current outer loop and the array generated at our current innner loop as our array of cities
+        let currentPath = hk(cities, start, distance_matrix, memoization);
+            
+        // if path returned to current path by our helper function is less than our current shortest path, place in shortest path
+        if (currentPath < shortestPath) {
+            shortestPath = currentPath;
+        }
+        
+    }
+
+    return shortestPath;
 }
+
+let dm = [[0,3,4,2,7],
+[3,0,4,6,3],
+[4,4,0,5,8],
+[2,6,5,0,6],
+[7,3,8,6,0]];
+
+console.log(tsp_hk(dm));
